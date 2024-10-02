11883e09654024945c6195d2de1b9c52856a2cdb
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-10-01 21:34:45 -0500

    move it to js file and images

---
+++ pages/blog.js
@@ -0,0 +1,47 @@
+import { posts } from "./json/blogposts.js";
+
+for (let index = 0; index < posts.length; index++) {
+  const post = posts[index];
+
+  var title = document.createElement("h1");
+  var description = document.createElement("h3");
+  var date = document.createElement("h3");
+  var tags = document.createElement("div");
+  var blog = document.createElement("div");
+
+  title.innerHTML = post.title + " by " + post.author;
+  description.innerHTML = post.description;
+  date.innerHTML = post.date;
+
+  for (let index = 0; index < post.tags.length; index++) {
+    const tag = post.tags[index];
+
+    var curTag = document.createElement("p");
+    curTag.innerHTML = "#" + tag;
+    // tags.append(curTag);
+  }
+
+  var lineEl = document.createElement("p");
+
+  for (let index = 0; index < post.blog.length; index++) {
+    const line = post.blog[index];
+
+    lineEl.innerHTML += line + "</p><p>";
+  }
+
+  blog.append(lineEl);
+
+  var posDiv = document.getElementById("posts");
+  posDiv.append(title);
+  posDiv.append(description);
+  posDiv.append(date);
+  // document.append(tags);
+  posDiv.append(blog);
+
+  if (post.img_url != undefined) {
+    var image = document.createElement("img");
+    image.src = "/School/" + post.img_url + ".png";
+
+    posDiv.append(image);
+  }
+}

604530341d1077b858b03005f3c8ad81611fd3a4
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-10-01 21:37:56 -0500

    dupes!?!?!?!?!?!?!?

--- pages/json/blogposts.js
+++ pages/json/blogposts.js
@@ -1,47 +1,6 @@
 export const posts = [
-    {
-      title: "second blog lolz!",
-      description: "obvious",
-      date: "October 1st 2024",
-      author: "IdealistCat",
-      tags: ["tester"],
-      blog: ["IMAGE!??!?!?!?!?"],
-      img_url: "blogStuff/testerImage",
-    },
-    {
-      title: "second blog lolz!",
-      description: "obvious",
-      date: "October 1st 2024",
-      author: "IdealistCat",
-      tags: ["unserious", "tester"],
-      blog: [
-        "fucking around and finding out at its finest lol",
-        "fucking lorem and finding out at its finest lol",
-        "fucking around and ipsum out at its finest lol",
-        "deiet around and finding out at its finest lol",
-        "fucking around and finding amet at its finest lol",
-        "defenitely got that wrong.",
-      ],
-      img_url: undefined,
-    },
-    {
-      title: "first blog lolz!",
-      description: "obvious",
-      date: "October 1st 2024",
-      author: "IdealistCat",
-      tags: ["unserious", "fpotm", "teset"], // fpotm = first post of the month
-      blog: [
-        "hello!!",
-        "why did i do this you are most likely asking.",
-        "Idk!",
-        "I have a cripling addiction to find an excuse for more web coding",
-        "LOL!",
-      ],
-      img_url: undefined,
-    },
-  ];var posts = [
   {
-    title: "second blog lolz!",
+    title: "third tester blog jesus!",
     description: "obvious",
     date: "October 1st 2024",
     author: "IdealistCat",
